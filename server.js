de4df623ffb7b1b33fb04a16b55069ae94eda41d
azizSharopov <sharopovaziz23@gmail.com>
2023-01-20 23:07:52 +0900

    loihani express qismini shakllantirib olish

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+let db;
+const connectionString =
+  "mongodb+srv://Ayden:CZuMXajOffcQyGs4@cluster0.kns1nwe.mongodb.net/Papay?retryWrites=true&w=majority";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3001;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
