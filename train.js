aadbbad455b125ec04a9549af285ed5ab78f2f87
azizSharopov <sharopovaziz23@gmail.com>
2023-02-02 00:27:07 +0900

    feat: L task

--- train.js
+++ train.js
@@ -1,5 +1,11 @@
 // K-task
-const foot = function (a, b, c) {
-  return a * 3 + b * 1 + c * 0;
-};
-console.log(foot(3, 4, 2));
+// const foot = function (a, b, c) {
+//   return a * 3 + b * 1 + c * 0;
+// };
+// console.log(foot(3, 4, 2));
+
+// L task
+let a = 4;
+let b = 5;
+const calculate = "a*b";
+console.log(eval(calculate));
