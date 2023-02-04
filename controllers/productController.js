fd3e8514f03fc6d6a996cfeabce7ced360c08bc5
azizSharopov <sharopovaziz23@gmail.com>
2023-02-04 16:45:56 +0900

    feat: develop restaurant users editing their products

--- controllers/productController.js
+++ controllers/productController.js
@@ -41,7 +41,16 @@ productController.addNewProduct = async (req, res) => {
 productController.updateChosenProduct = async (req, res) => {
   try {
     console.log("POST: cont/updateChosenProduct");
+    const product = new Product();
+    const id = req.params.id;
+    const result = await product.updateChosenProductData(
+      id,
+      req.body,
+      req.member.id
+    );
+    await res.json({ state: "sucess", data: result });
   } catch (err) {
     console.log(`ERROR, cont/updateChosenProduct, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
   }
 };
