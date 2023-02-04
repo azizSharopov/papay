328956b5bdd300b2f0f9ef61d6a2b6762d45a729
azizSharopov <sharopovaziz23@gmail.com>
2023-02-04 18:29:00 +0900

    feat: class Member

--- train.js
+++ train.js
@@ -4,8 +4,29 @@
 // };
 // console.log(foot(3, 4, 2));
 
+const { indexInformation } = require("mongodb/lib/operations/db_ops");
+
 // L task
-let a = 4;
-let b = 5;
-const calculate = "a*b";
-console.log(eval(calculate));
+// let a = 4;
+// let b = 5;
+// const calculate = "a*b";
+// console.log(eval(calculate));
+
+// class Member
+class Member {
+  #counts;
+  went;
+  constructor(a, b) {
+    this.#counts = a;
+    this.went = b;
+  }
+  addMember(a) {}
+  removeMember(b) {}
+  inform() {
+    this.info = this.#counts - this.went;
+    console.log(this.info);
+  }
+}
+
+const myinfo = new Member(5, 2);
+myinfo.inform();
