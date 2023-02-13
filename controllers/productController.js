86c3e8b338d1827508b9c538879fc8b72103ca1a
azizSharopov <sharopovaziz23@gmail.com>
2023-02-14 01:41:17 +0900

    fix: develop restaurant related product control page on adminka

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,7 +28,7 @@ productController.addNewProduct = async (req, res) => {
     const result = await product.addNewProductData(data, req.member);
 
     const html = `<script>
-                    alert(new product added successfully);
+                    alert('new product added successfully');
                     window.location.replace('/resto/products/menu');
                   </script>`;
 
@@ -48,7 +48,7 @@ productController.updateChosenProduct = async (req, res) => {
       req.body,
       req.member.id
     );
-    await res.json({ state: "sucess", data: result });
+    await res.json({ state: "success", data: result });
   } catch (err) {
     console.log(`ERROR, cont/updateChosenProduct, ${err.message}`);
     res.json({ state: "fail", message: err.message });
